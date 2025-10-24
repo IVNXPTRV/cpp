820c00f89ca378476c738075ebbbbf4c809b722a
Ivan Petrov <ivnxptrv@frutro.localdomain>
2025-10-24 02:20:29 -0600

    cpp0/ex00 done

---
+++ modules/cpp0/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    std::string line;
+
+    if (argc == 1)
+    {
+        line = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::cout << line;
+    }
+    while(*++argv)
+    {
+        line = *argv;      
+        for (size_t i = 0; i < line.length(); i++)
+            line[i] = std::toupper(line[i]);
+        std::cout << line << " ";
+    }
+    std::cout << std::endl;
+}
