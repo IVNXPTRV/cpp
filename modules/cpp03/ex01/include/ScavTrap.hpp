a111a2277197bab57113fa25d3ace7da997eab33
Ivan Petrov <ivnxptrv@frutro.localdomain>
2025-11-02 21:34:07 -0700

    add ex01 finished

---
+++ modules/cpp03/ex01/include/ScavTrap.hpp
@@ -0,0 +1,30 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <string>
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+ public:
+  ScavTrap();
+  ScavTrap(const ScavTrap& other);
+  ScavTrap& operator=(const ScavTrap& other);
+  ~ScavTrap();
+
+  ScavTrap(std::string name);
+
+  static const std::string defaultModel;
+  static const std::string defaultName;
+  static const unsigned int defaultHealth;
+  static const unsigned int defaultEnergy;
+  static const unsigned int defaultDamage;
+
+  void attack(
+      const std::string& target);  // target lose <damage> of <health>; cost 1
+  void guardGate();
+};
+
+std::ostream& operator<<(std::ostream& out, const ScavTrap& right);
+
+#endif
---
+++ modules/cpp03/ex01/src/main.cpp
@@ -0,0 +1,49 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "ScavTrap.hpp"
+
+int main() {
+  ScavTrap trap0("Foo");
+  ScavTrap trap1("Bar");
+
+  trap0.beRepaired(0);
+  trap0.attack(trap1.getName());
+  trap1.takeDamage(ScavTrap::defaultDamage);
+  trap0.attack(trap1.getName());
+  trap1.takeDamage(5);
+  trap1.beRepaired(4);
+  trap1.attack(trap0.getName());
+  trap0.takeDamage(5);
+  trap0.beRepaired(5000);
+  trap1.beRepaired(5000);
+  std::cout << trap1 << std::endl;
+  trap1.attack(trap1.getName());
+  trap1.attack(trap1.getName());
+  trap1.attack(trap1.getName());
+  trap1.attack(trap1.getName());
+  trap1.attack(trap1.getName());
+  trap1.attack(trap1.getName());
+  trap1.takeDamage(5000);
+  trap1.beRepaired(5000);
+  trap1.takeDamage(1);
+  trap1.attack(trap1.getName());
+  std::cout << trap1 << std::endl;
+  std::cout << trap0 << std::endl;
+  trap0.takeDamage(10000);
+  std::cout << trap0 << std::endl;
+  trap0.takeDamage(101111);
+  std::cout << trap0 << std::endl;
+  trap0.beRepaired(100);
+  std::cout << trap0 << std::endl;
+
+  ScavTrap trap2;
+  std::cout << trap2 << std::endl;
+  ScavTrap trap3;
+  trap3.setName("Waz");
+  trap2 = trap3;
+  std::cout << trap2 << std::endl;
+  std::cout << trap3 << std::endl;
+
+  return EXIT_SUCCESS;
+}
