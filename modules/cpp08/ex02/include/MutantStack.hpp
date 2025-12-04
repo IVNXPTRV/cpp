512819bd1bd809246fc08c77fc7ec443fe866435
Ivan Petrov <ivnxptrv@frutro.localdomain>
2025-12-03 22:07:07 -0700

    feat: add cpp08/ex02

---
+++ modules/cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,50 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T, typename C = std::deque<T> >
+class MutantStack : public std::stack<T, C> {
+ public:
+  typedef typename std::stack<T, C>::container_type::iterator iterator;
+  typedef
+      typename std::stack<T, C>::container_type::const_iterator const_iterator;
+  typedef typename std::stack<T, C>::container_type::reverse_iterator
+      reverse_iterator;
+  typedef typename std::stack<T, C>::container_type::const_reverse_iterator
+      const_reverse_iterator;
+
+  MutantStack<T, C>(void) : std::stack<T, C>(){};
+  MutantStack<T, C>(MutantStack<T, C> const& src) : std::stack<T, C>(src){};
+  ~MutantStack<T, C>(void){};
+
+  MutantStack<T, C>& operator=(MutantStack<T, C> const& src) {
+    if (this != &src) this->c = src.c;
+    return (*this);
+  }
+
+  MutantStack<T, C>::iterator begin(void) { return (this->c.begin()); }
+
+  MutantStack<T, C>::iterator end(void) { return (this->c.end()); }
+
+  MutantStack<T, C>::const_iterator begin(void) const {
+    return (this->c.begin());
+  }
+
+  MutantStack<T, C>::const_iterator end(void) const { return (this->c.end()); }
+
+  MutantStack<T, C>::reverse_iterator rbegin(void) {
+    return (this->c.rbegin());
+  }
+
+  MutantStack<T, C>::reverse_iterator rend(void) { return (this->c.rend()); }
+
+  MutantStack<T, C>::const_reverse_iterator rbegin(void) const {
+    return (this->c.rbegin());
+  }
+
+  MutantStack<T, C>::const_reverse_iterator rend(void) const {
+    return (this->c.rend());
+  }
+};
+#endif
